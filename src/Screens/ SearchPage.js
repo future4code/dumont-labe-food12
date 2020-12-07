1de5c2d81d682973ba96cc946712847327db70bd
Felipe Polato <fepolato@hotmail.com>
2020-12-07 14:18:20 -0300

    searchPage

---
+++ src/Screens/ SearchPage.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function SearchPage() {
+  const [search, setSearch] = useState();
+  const [restaurante, setRestaurante] = useState([
+    "burguer",
+    "bar do zé",
+    "lasanha",
+  ]);
+
+  const verificaRestaurante = () => {
+    if (search === restaurante) {
+      return <p>Restaurante existe</p>;
+    }
+  };
+
+  const handleInput = (e) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div>
+      {verificaRestaurante}
+      <input value={search} type="text" placeholder="Buscar" />
+      <button onClick={handleInput}>Buscar</button>
+      <p>Busque Por Nomes De Restaurantes</p>
+    </div>
+  );
+}
+
+export default SearchPage;
