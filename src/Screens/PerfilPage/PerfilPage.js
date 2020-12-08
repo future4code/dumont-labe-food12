267d598aef9e92b53f04251f64fd69f6089b8c88
laurasanches <lalinhabom@gmail.com>
2020-12-08 17:24:01 -0300

    começo da estilização perfil

--- src/Screens/PerfilPage/PerfilPage.js
+++ src/Screens/PerfilPage/PerfilPage.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { MainDiv, Header, HeaderTitleDiv, AdressInfoDiv, DeliveryAdress, AdressDetail, Title, Pencil } from './styles';
+import CreateIcon from '@material-ui/icons/Create';
 
 function PerfilPage() {
     return(
-        <div>
-            PerfilPage
-        </div>
+        <MainDiv>
+            <Header>
+                <HeaderTitleDiv>
+                    <p>Meu perfil</p>
+                </HeaderTitleDiv>
+            </Header>
+            <hr/>
+            <div>
+                <Pencil>Bruna Oliveira <CreateIcon/></Pencil>
+                <p>bruna.o@gmail.com</p>
+                <p>3333-3333</p>
+            </div>
+            <AdressInfoDiv>
+                <DeliveryAdress>
+                    Endereço de entrega <CreateIcon/>
+                </DeliveryAdress>
+                <AdressDetail>
+                    Rua Labenu, 100
+                </AdressDetail>
+            </AdressInfoDiv>
+            <Title>
+                Histórico de pedidos
+            </Title>
+            <hr/>
+        </MainDiv>
     )
 }
 
---
+++ src/Screens/PerfilPage/styles.js
@@ -0,0 +1,75 @@
+import styles from "styled-components"
+
+export const MainDiv = styles.div`
+    width: 360px;
+    height: 640px;
+`
+
+export const Header = styles.header`
+    display: flex;
+    width: 360px;
+    height: 64px;
+    justify-content: center;
+    align-items: center;
+`
+
+export const HeaderTitleDiv = styles.div`
+    display: flex;
+    justify-content: center;
+    width: 175px;
+    height: 44px;
+`
+
+export const AdressInfoDiv = styles.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 360px;
+    height: 76px;
+    background-color: #eeeeee;
+`
+
+export const DeliveryAdress = styles.p`
+    display: flex;
+    width: 328px;
+    height: 18px;
+    margin: 0 0 8px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: grey;
+    justify-content: space-between;
+`
+
+export const AdressDetail = styles.p`
+    display: flex;
+    width: 328px;
+    height: 18px;
+    margin: 8px 0 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: #000000;
+`
+
+export const Title = styles.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 360px;
+    height: 42px;
+    margin-top: 8px;
+`
+
+export const Pencil = styles.p`
+    display: flex;
+    justify-content: space-between;
+`
