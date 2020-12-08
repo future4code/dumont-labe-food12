2369c66348c1f81e1fba9d40d6071930996791cf
Felipe Polato <fepolato@hotmail.com>
2020-12-08 11:21:40 -0300

    signUp com u maiusculo

--- src/route/Routers.js
+++ src/route/Routers.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginPage from "../Screens/LoginPage/LoginPage";
-import SignupPage from "../Screens/SignupPage/SignupPage";
+import SignUpPage from "../Screens/SignUpPage/SignUpPage";
 import FeedPage from "../Screens/FeedPage/FeedPage";
 import RegisterAddressPage from "../Screens/RegisterAddressPage/RegisterAddressPage";
 import RestaurantPage from "../Screens/RestaurantPage/RestaurantPage";
@@ -21,7 +21,7 @@ function Routers() {
           <LoginPage />
         </Route>
         <Route exact path="/signup">
-          <SignupPage />
+          <SignUpPage />
         </Route>
         <Route exact path="/signup/address">
           <RegisterAddressPage />
