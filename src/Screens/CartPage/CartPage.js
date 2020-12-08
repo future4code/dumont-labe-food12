c79643f6e3fc351574c658e567b45ba32cc2e6e8
Augusto Luna <caugustolunas@gmail.com>
2020-12-08 17:50:25 -0300

    Tentando solucionar a parte lógica do projeto

--- src/Screens/CartPage/CartPage.js
+++ src/Screens/CartPage/CartPage.js
@@ -1,14 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { MainDiv, Header, HeaderTitleDiv, AdressInfoDiv, DeliveryAdress, AdressDetail, Title, SubtotalDiv, SubtotalTitle, SubtotalValue, ShippingDiv, PaymentMethodDiv, PaymentMethodTitle, Hr, ConfirmPayButton, CartFooter, GoHomeDiv, GoCartDiv, GoProfileDiv } from './styled';
 
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
+import axios from 'axios';
 
 function CartPage() {
+    const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
+    const [priceToPay, setPriceToPay] = useState(0)
+
+    useEffect(() => {
+       getRestaurantDetails()
+      }, []);
+
+    const getRestaurantDetails = (() => {
+        const body = {
+                "products": [{
+                    "id": "CnKdjU6CyKakQDGHzNln",
+                    "quantity": 10
+                }, {
+                    "quantity": 1,
+                    "id": "KJqMl2DxeShkSBevKVre"
+                }],
+                "paymentMethod": "creditcard"
+        }
+
+        axios.post("https://us-central1-missao-newton.cloudfunctions.net/futureEatsA/restaurants/1/order", {headers:{
+            auth: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlpkYWcyZVBHOG1zUDNkdG1vRFdrIiwibmFtZSI6IkFzdHJvZGV2IiwiZW1haWwiOiJhc3Ryb2RldkBmdXR1cmU0LmNvbSIsImNwZiI6IjExMy4xMTEuMTExLTExIiwiaGFzQWRkcmVzcyI6dHJ1ZSwiYWRkcmVzcyI6IlIuIEFmb25zbyBCcmF6LCAxNzcsIDcxIC0gVmlsYSBOLiBDb25jZWnDp8OjbyIsImlhdCI6MTYwNzM3NTY5Mn0.ODZDt8sX_emUKT1x-FxDcP0eWWI5z1yi5lYDTsak78s"
+        }} ).then((response) => {
+            setProducts(response.data.restaurant.products)
+            console.log(response)
+        }).catch((error) => {
+            alert("Errou!")
+            console.error(error)
+        })
+
+    })
     return(
         <MainDiv>
+            {products && products.map((item) => {
+                return(
+                    <div>
+                        <p>{item.price}</p>
+                        <p>{item.category}</p>
+                        <p>{item.name}</p>
+                        <p>{item.description}</p>
+                        <img src={item.photoUrl}/>
+                    </div>
+                )
+            })}
             <Header>
                 <HeaderTitleDiv>
                     <p>Meu Carrinho</p>
