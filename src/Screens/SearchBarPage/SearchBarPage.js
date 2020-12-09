3b5ed12f1c7eb977a9c703f988fbe17f24e1c05c
Felipe Polato <fepolato@hotmail.com>
2020-12-09 12:22:48 -0300

    search com token chumbado

--- src/Screens/SearchBarPage/SearchBarPage.js
+++ src/Screens/SearchBarPage/SearchBarPage.js
@@ -12,33 +12,35 @@ function SearchBarPage() {
   useEffect(() => {
     getListaRestaurants();
   }, []);
-  
-  // const token = localStorage.getItem("token");
-  
+
   const getListaRestaurants = () => {
+    // const token = localStorage.getItem("token");
+
+    // console.log("token", token);
+
     axios
-    .get(
-      `https://us-central1-missao-newton.cloudfunctions.net/futureEatsA/restaurants`,
-      {
-        headers: {
-          auth: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlpkYWcyZVBHOG1zUDNkdG1vRFdrIiwibmFtZSI6IkFzdHJvZGV2IiwiZW1haWwiOiJhc3Ryb2RldkBmdXR1cmU0LmNvbSIsImNwZiI6IjExMy4xMTEuMTExLTExIiwiaGFzQWRkcmVzcyI6dHJ1ZSwiYWRkcmVzcyI6IlIuIEFmb25zbyBCcmF6LCAxNzcsIDcxIC0gVmlsYSBOLiBDb25jZWnDp8OjbyIsImlhdCI6MTYwNzM3NTY5Mn0.ODZDt8sX_emUKT1x-FxDcP0eWWI5z1yi5lYDTsak78s",
-        },
-      }
+      .get(
+        `https://us-central1-missao-newton.cloudfunctions.net/futureEatsA/restaurants`,
+        {
+          headers: {
+            auth: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlpkYWcyZVBHOG1zUDNkdG1vRFdrIiwibmFtZSI6IkFzdHJvZGV2IiwiZW1haWwiOiJhc3Ryb2RldkBmdXR1cmU0LmNvbSIsImNwZiI6IjExMy4xMTEuMTExLTExIiwiaGFzQWRkcmVzcyI6dHJ1ZSwiYWRkcmVzcyI6IlIuIEFmb25zbyBCcmF6LCAxNzcsIDcxIC0gVmlsYSBOLiBDb25jZWnDp8OjbyIsImlhdCI6MTYwNzM3NTY5Mn0.ODZDt8sX_emUKT1x-FxDcP0eWWI5z1yi5lYDTsak78s",
+          },
+        }
       )
       .then((response) => {
         setRestaurants(response.data.restaurants);
       })
       .catch((error) => {
-        alert("Erro ao requisitar os restaurantes");
+        console.log(error.messenge);
       });
-    };
-    
-    console.log(restaurants);
-    
-    const onSubmit = (event) => {
-      console.log("entrou");
-      setFilteredRestaurants(
-        restaurants.restaurants &&
+  };
+
+  console.log(restaurants);
+
+  const onSubmit = (event) => {
+    console.log("entrou");
+    setFilteredRestaurants(
+      restaurants.restaurants &&
         restaurants.restaurants.filter((restaurant) => {
           if (restaurant.name === form.search) {
             return true;
@@ -46,28 +48,34 @@ function SearchBarPage() {
             return false;
           }
         })
-        );
-      };
-     
-      
-      const filteredRestaurantsCards =
-      filteredRestaurants &&
-      filteredRestaurants.map((restaurant) => {
-        return <div>{restaurant.name}</div>;
-      });
-      
-      return (
-        <div>
+    );
+  };
+
+  return (
+    <div>
       <form>
         <Input
           placeholder="Busca"
           onChange={onChange}
           value={form.search}
           name={"search"}
-        />
+          />
         <Button onClick={onSubmit}>Buscar</Button>
       </form>
-      {filteredRestaurantsCards}
+
+      {filteredRestaurants &&
+        filteredRestaurants.map((restaurant) => {
+          console.log(restaurant)
+          return (
+            <div>
+              <p>{restaurant.address}</p>
+              <p>{restaurant.category}</p>
+              <p>{restaurant.deliveryTime}</p>
+              <p>{restaurant.name}</p>
+              <p>{restaurant.logoUrl}</p>
+            </div>
+          );
+        })}
     </div>
   );
 }
