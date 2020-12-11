670d00d662e8ab635249eeb17805b297565800ff
Noh Ah Jeong <noh_ah@hotmail.com>
2020-12-11 17:13:57 -0300

    finalzim

--- src/Screens/EditPerfilPage/styles.js
+++ src/Screens/EditPerfilPage/styles.js
@@ -4,8 +4,6 @@ export const EditPerfilContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 360px;
-  height: 640px;
   margin: 10px;
 `;
 
--- src/Screens/PerfilPage/styles.js
+++ src/Screens/PerfilPage/styles.js
@@ -1,90 +1,35 @@
 import styled from "styled-components";
 
-export const MainDiv = styled.div`
-  width: 360px;
-  height: 100px;
-`;
-
-export const Header = styled.header`
-  display: flex;
-  width: 360px;
-  height: 64px;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const AdressInfoDiv = styled.div`
+export const ProfileContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   width: 360px;
-  height: 76px;
-  margin: 16px 0;
-  padding: 16px;
-  background-color: #eeeeee;
-`;
-
-export const DeliveryAdress = styled.p`
-  display: flex;
-  width: 328px;
-  height: 18px;
-  margin: 0 0 8px;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  font-size: 16px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: -0.39px;
-  color: grey;
-  justify-content: space-between;
-`;
-
-export const AdressDetail = styled.p`
-  display: flex;
-  width: 328px;
-  height: 18px;
-  margin: 8px 0 0;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  font-size: 16px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: -0.39px;
-  color: #000000;
-`;
-
-export const Title = styled.div`
-  display: flex;
+  height: 640px;
   align-items: center;
-  justify-content: center;
-  width: 360px;
-  height: 42px;
-  margin-top: 8px;
 `;
 
-export const Pencil = styled.p`
+export const ProfileDetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 360px;
+  padding: 16px;
+`;
+
+export const RowFlexContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
-export const GoEditProfileDiv = styled.div`
-  cursor: pointer;
-  outline: none;
-
-  &:hover {
-    color: #5cb646;
-  }
+export const AddressDetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #B8B8B8;
+  box-sizing: border-box;
+  width: 360px;
+  padding: 16px;
 `;
 
-export const GoEditAddressDiv = styled.div`
-  cursor: pointer;
-  outline: none;
-
-  &:hover {
-    color: #5cb646;
-  }
+export const OrderHistoryContainer = styled.div`
 `;
